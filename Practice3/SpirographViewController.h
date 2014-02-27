8f5abcb10063274deba059c23663679612f2a188
Michael Vitone <vitone@ravenclaw.cs.moravian.edu>
2014-02-27 16:04:16 -0500

    i hope it all works

--- Practice3/SpirographViewController.h
+++ Practice3/SpirographViewController.h
@@ -7,7 +7,28 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "SpirographView.h"
+#import "HarmonigraphView.h"
 
-@interface SpirographViewController : UIViewController
+@interface SpirographViewController : UIViewController <UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet HarmonigraphView *harmonigraphView;
+
+@property (weak, nonatomic) IBOutlet UISlider *lSlider;
+
+@property (weak, nonatomic) IBOutlet UISlider *kSlider;
+
+@property (weak, nonatomic) IBOutlet SpirographView *spirographView;
+
+@property (weak, nonatomic) IBOutlet UITextField *stepSizeTextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *numberOfStepsTextField;
+
+- (IBAction)lChanged:(id)sender;
+- (IBAction)kChanged:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *overWriteSegmentControl;
+
+- (IBAction)redraw:(id)sender;
 
 @end
